ec9c6502aac793140981e3a4712b40b578ee66b1
Olaf Klischat <o.klischat@syseleven.de>
2015-11-27 01:06:14 +0100

    more dev packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password password roo
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password rootpw'
 
 sudo apt-get update
-sudo apt-get install -y python-dev ipython mysql-server mysql-client libmysqlclient-dev git vim emacs python-pip ruby-dev
+sudo apt-get install -y python-dev ipython mysql-server mysql-client libmysqlclient-dev git vim emacs python-pip ruby-dev build-essential devscripts automake dh-make dpkg-dev debhelper dh-autoreconf sgmltools-lite dpkg w3m po4a libdebconfclient0-dev
 
 pip install virtualenv
 
