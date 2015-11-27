b87902e4a8b0be18354da1c09ac1ed72755070a6
Olaf Klischat <o.klischat@syseleven.de>
2015-11-27 21:25:05 +0100

    more dev packages; git, ~/utils initialization

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,20 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password password roo
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password rootpw'
 
 sudo apt-get update
-sudo apt-get install -y python-dev ipython mysql-server mysql-client libmysqlclient-dev git vim emacs python-pip ruby-dev build-essential devscripts automake dh-make dpkg-dev debhelper dh-autoreconf sgmltools-lite dpkg w3m po4a libdebconfclient0-dev
+sudo apt-get install -y python-dev ipython mysql-server mysql-client libmysqlclient-dev git vim emacs python-pip ruby-dev build-essential gdb devscripts automake dh-make dpkg-dev debhelper dh-autoreconf sgmltools-lite dpkg w3m po4a libdebconfclient0-dev
 
 pip install virtualenv
 
+# TODO git clone home
+
+git config --global user.email "o.klischat@syseleven.de"
+git config --global user.name "Olaf Klischat"
+
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+
+HH=/home/hostolaf
+
+if [ -d "$HH/utils" ]; then
+    cd "$HH/utils"
+    make  # TODO make it abort when interactive input (password entry) is required?
+fi
